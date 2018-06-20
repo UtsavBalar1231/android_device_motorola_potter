fd2107962732a4f6bb752bccf812eae0cabf6b87
Dan Willemsen <dwillemsen@google.com>
2018-06-19 22:28:20 -0700

    potter: build have overriding commands
    These products have warnings about overriding build commands, which
    means that there are multiple instructions on how to build a file, and
    we've just picked one of them. Which one we pick is usually stable, but
    it can change.
    
    Bug: 77611511
    Test: turn default to false, walleye still uses a warning
    Change-Id: I11d194863b313f64b19291f65219682b169d9ced
    Signed-off-by: UtsavisGreat <utsavbalar1231@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -52,6 +52,8 @@ TARGET_2ND_CPU_ABI2 := armeabi
 TARGET_2ND_CPU_VARIANT := cortex-a53
 TARGET_USES_64_BIT_BINDER := true
 
+BUILD_BROKEN_DUP_RULES := true
+
 # Asserts
 TARGET_OTA_ASSERT_DEVICE := potter,potter_retail
 
